76b4a16559af3b0c89b7af969e51a2e3f6daa693
lokaimoma <kelvinclark2k77@gmail.com>
2022-08-26 17:26:59 +0100

    Complete benefits section

--- pages/views/services-for-paid-social-media-ad.tsx
+++ pages/views/services-for-paid-social-media-ad.tsx
@@ -240,7 +240,7 @@ const Page: NextPage = () => {
       {/* advertisment services section */}
       <div>
         <div className="py-28">
-          <h3 className="text-center text-3xl">
+          <h3 className="text-center text-4xl">
             Services for Social Media Advertising for Every Platform{" "}
           </h3>
         </div>
@@ -259,6 +259,57 @@ const Page: NextPage = () => {
             </div>
           </Section>
         </div>
+      </div>
+
+      {/* benefits section */}
+      <div className="py-20">
+        <Section>
+          <h2 className="text-4xl">
+            Top-Rated Paid Social Advertising Services
+          </h2>
+
+          <p className="pt-10">
+            Our social media ad management services go much further than simple
+            campaign management. We take it a step further. Our team assists
+            yours in achieving its goals, from increasing likes to increasing
+            sales, while also optimizing your ad spend for the best return on
+            investment (ROI).
+          </p>
+
+          <p className="pt-10">
+            Your company can benefit from the following seven advantages by
+            using our social media advertising services:
+          </p>
+
+          <ul className="pt-10 list-decimal ml-4">
+            <li>Reach the people who are most important to your company.</li>
+            <li>Retarget site visitors, email subscribers, and others.</li>
+            <li>
+              For data-driven targeting and messaging, use artificial
+              intelligence and machine learning.
+            </li>
+            <li>
+              Create cost-effective, results-driven campaigns that meet your
+              advertising objectives.
+            </li>
+            <li>
+              Optimize your advertising spend to maximize your ROI and results.
+            </li>
+            <li>
+              Increase brand awareness among your target market's most valuable
+              users.
+            </li>
+            <li>
+              Increase customer loyalty for repeat sales, word-of-mouth
+              referrals, and more.
+            </li>
+          </ul>
+
+          <p className="pt-10">
+            Empower your company with social media ad services designed for
+            efficiency and success.
+          </p>
+        </Section>
       </div>
     </div>
   );
