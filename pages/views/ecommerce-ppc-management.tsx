2542667a58acc3f42700add321d98e85258ad284
saif0307 <saif.1077@yahoo.com>
2022-08-31 00:43:06 +0500

    changes

--- pages/views/ecommerce-ppc-management.tsx
+++ pages/views/ecommerce-ppc-management.tsx
@@ -1,36 +1,29 @@
-import React from "react";
-import { NextPage } from "next";
-import { Hero, Section } from "components/Fawwaz/Sections";
-import SEOMarketingSVG from "components/assets/svg/SEOMarketingSVG";
-import Logo from "components/Fawwaz/Logo";
-import logo from "components/assets/logo/logo1.png";
-
- const Page: NextPage = () => {
-
-	return (<div>
-        {/* First Section */}
-			<div className=" bg-black-70 px-3 lg:px-0">
-				<Hero
-					title="Ecommerce PPC Management Services: Give your ROAS a Workout!"
-					description="People are now shopping online, and pay-per-click (PPC) ads can help you catch up. Muscled PPC Management Services will allow your store to use Google and Bing to get traffic and make sales. Why not see how much we can help you by requesting a free ecommerce PPC proposal today?"
-					heroSVG={<SEOMarketingSVG className="w-80 lg:w-full" />}
-					input
+import type { NextPage } from "next";
+import Head from "next/head";
+import LandingPage from "../../components/landingPage/LandingPage";
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<meta property="og:image" content="https://example.com/ogp.jpg" />
+				<meta
+					property="og:image:secure_url"
+					content="https://secure.example.com/ogp.jpg"
 				/>
-			</div>
-		{/* Our partner section */}
-		<Section className="text-center pt-16">
-            <h3 className="text-black-50 text-2xl">Our Partners</h3>
+				<meta property="og:image:type" content="image/jpeg" />
+				<meta property="og:image:width" content="400" />
+				<meta property="og:image:height" content="300" />
+				<meta
+					property="og:image:alt"
+					content="A shiny red apple with a bite taken out"
+				/>
+				<title>
+					Ecommerce PPC management services | Ecommerce PPC management services
+				</title>
+			</Head>
+			<LandingPage />
+		</>
+	);
+};
 
-            <div className="pt-5 flex flex-wrap justify-between pb-12 gap-6">
-                <Logo src={logo.src} width={154} height={34} />
-                <Logo src={logo.src} width={154} height={34} />
-                <Logo src={logo.src} width={154} height={34} />
-                <Logo src={logo.src} width={154} height={34} />
-                <Logo src={logo.src} width={154} height={34} />
-            </div>
-        </Section>
-    </div>)
-}
-
-
-export default Page
+export default Home;
