362eb92251ae70172c6b4bc765588d9a2a30c237
imtiazpy <ahmed.imti1994@gmail.com>
2022-09-01 12:52:16 +0600

    added Testimonials section

---
+++ components/Fawwaz/Sections/LandingTestimonials.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import Button from "../Button";
+import VideoFrame from "components/Fawwaz/VideoFrame";
+
+
+const LandingTestimonials = () => {
+	return (
+		<div className="bg-black testimonial-section">
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
+				<div className="text-accent-white">
+					<p className="text-lg md:text-2xl mb-8">
+                    ‘’Muscled continues to provide advanced solutions that add value to my company and my clients on a consistent basis. Muscled is worth the investment, easily outpacing the competition!"
+					</p>
+
+					<p className="font-bold text-xl md:text-2xl mb-8">Owner</p>
+
+					<p className="font-bold text-base md:text-xl mb-14">
+						Company Name
+					</p>
+
+					<Button bgColor="bg-[#C40000]" textColor="text-white">
+						See Our Client's Testimonials
+					</Button>
+				</div>
+				<div className="min-h-[400px] h-full bg-white-gray relative">
+					<VideoFrame className="w-full h-full absolute" allowFullScreen />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LandingTestimonials;
--- pages/views/landing-page-design-services.tsx
+++ pages/views/landing-page-design-services.tsx
@@ -6,23 +6,34 @@ import Hero from '../../components/Fawwaz/Sections/Hero';
 import OurPartners from '../../components/Fawwaz/Sections/OurPartners';
 import PricingPlan from '../../components/Fawwaz/Sections/PricingPlan';
 import PromotionOne from '../../components/Fawwaz/Sections/PromotionOne';
+import LandingTestimonials from 'components/Fawwaz/Sections/LandingTestimonials';
 
  const Page: NextPage = () => {
 
 	return (
         <>
-            <Hero 
-                title={"Landing Page Design Services for PPC Campaigns"} 
-                description = {''} 
-                // heroSVG = {FunnelSVG}
-                input 
-            />
-            <OurPartners />
-            <PricingPlan />
+            <section>
+                <Hero 
+                    title={"Landing Page Design Services for PPC Campaigns"} 
+                    description = {''} 
+                    // heroSVG = {FunnelSVG}
+                    input 
+                />
+            </section>
+            <section>
+                <OurPartners />
+            </section>
+            <section>
+                <PricingPlan />
+            </section>
             <section>
                 <PromotionOne 
                     title='Improving your sales with Landing Page Design Services'
                 />
+            </section>
+
+            <section>
+                <LandingTestimonials />
             </section>
         </>
     )
