8033fc99aed9805cff72092f735dba13437ebdac
Saqlain1999 <saqlain_shah99@hotmail.com>
2022-09-05 08:53:03 -0400

    removed errors

--- components/SocialAdvertisingServices/components/questions/tableOfContents.tsx
+++ components/SocialAdvertisingServices/components/questions/tableOfContents.tsx
@@ -16,20 +16,20 @@ const TableOfContents = () => {
   }
   return (
     <div className="flex flex-col justify-between items-center bg-white rounded-xl sticky top-5">
-      <p className="py-4 px-12 rounded-t-xl bg-[#3E5661] text-white w-full flex justify-between items-center space-x-4">
+      <div className="py-4 px-12 rounded-t-xl bg-[#3E5661] text-white w-full flex justify-between items-center space-x-4">
         <div className="flex items-center gap-x-4">
           {/* <Image src={ListIcon} alt="list" width={26} height={20} /> */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
             />
           </svg>
@@ -39,17 +39,17 @@ const TableOfContents = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-6 h-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
           />
         </svg>
-      </p>
+      </div>
       <List
         content={contents}
         className="text-[#5F27BD] underline space-y-4 px-4 py-8 ml-12"
--- components/SocialAdvertisingServices/data/hero.ts
+++ components/SocialAdvertisingServices/data/hero.ts
@@ -5,7 +5,7 @@ import png4 from '../../assets/SocialAdvertisingServices/icon-cloud-computing 1.
 
 export const HeroSectionData = {
   title:
-    'Attract, engage and sell\nwith Muscled´s D2C\nSocial Advertisement\nServices',
+    'Attract, engage and sell with Muscled´s D2C Social Advertisement Services',
   description:
     "More than 5 billion people are active social media users turning social platforms into a great source to directly reach consumers (D2C). If you sell B2p products then social ad's will help to breach that gap with your consumers. Letting you and selling to them directly impacts your bottom line results. Keep reading to learn how Muscled can help you reach your goals with our direct to consumer social media advertising services!"
 }
