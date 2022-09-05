7b7d14ecc11d41fd49a9deb4be24c56a083402eb
saif0307 <saif.1077@yahoo.com>
2022-09-05 18:48:48 +0500

    changes

--- components/landingPage/FAQ/FAQ.tsx
+++ components/landingPage/FAQ/FAQ.tsx
@@ -8,13 +8,13 @@ export default function FAQ({}: Props) {
       question: "What are ecommerce PPC services?",
       answer:
         " Ecommerce PPC services are for advertising on pay-per-click ad networks, like Google Ads, Bing, Microsoft Advertising, Facebook, and many more. You are to expect the development, management, and monitoring of your ad campaigns.",
-      height: "2xl:h-[140px] md:h-[190px] h-[234px]",
+      height: "2xl:h-[140px] md:h-[190px] sm:h-[234px]",
     },
     {
       question: "How much do ecommerce PPC services cost?",
       answer:
         "Typically, ecommerce PPC plans cost $400 to $5000, it could also be 10 to 20% of your monthly ad spend. The price varies, depending on several factors like your intended ad spend or the PPC agency’s pricing.",
-      height: "2xl:h-[110px] md:h-[160px] h-[203px]",
+      height: "2xl:h-[110px] md:h-[160px] sm:h-[203px]",
     },
   ];
   return (
