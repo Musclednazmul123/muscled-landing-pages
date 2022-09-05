78941d131eb2d21117c4a877344174d3c2b730db
elassari19 <elassari19@gmail.com>
2022-09-05 22:10:50 +0100

    add pricing table

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+import Pricing from "./Pricing";
+import InputRange from "./InputRange";
+
+export { Pricing, InputRange };
--- pages/views/website-design-service.tsx
+++ pages/views/website-design-service.tsx
@@ -3,6 +3,7 @@ import { NextPage } from "next/";
 
 import { Section, Testimonial, Footer, Hero } from "components/Fawwaz/Sections";
 import Button from "components/Fawwaz/Button";
+import { InputRange, Pricing } from "components";
 import Logo from "components/Fawwaz/Logo";
 import image from "components/assets/image-14.png";
 import { Card } from "components/Fawwaz/Card";
@@ -84,6 +85,42 @@ const Page: NextPage = () => {
       </Section>
 
       <Testimonial />
+
+      {/* Pricing Table */}
+      <Section className="w-[996px] mx-auto my-20">
+        <Pricing
+          title={"Get a Free Quote Now Making use of our web design calculator"}
+          desc={`For a free, immediate quote, enter your needs into the sliders below and then click the "See Pricing" button. You'll get an overall range outlining how much it may cost to have such a website carried up because we\'ll give you both low and high end estimates for each service category.`}
+        >
+          <InputRange inputText="10 - 50" inputTitle="Amount of pages" />
+          <InputRange
+            inputTitle="Design"
+            inputText="Simple yet appealing"
+            width={310}
+            left={"15%"}
+          />
+          <InputRange
+            inputText="5 - 10"
+            inputTitle="Pages in a copywriting project"
+          />
+          <InputRange inputTitle="SEO " inputText="30 - 50" />
+          <InputRange inputTitle="Adaptive Design" inputText="Yes" left="40%" />
+          <InputRange inputTitle="Integration of databases" inputText="Basic" />
+          <InputRange inputTitle="Ecommerce capabilities" inputText="Simple" />
+          <InputRange inputTitle="CMS" inputText="Standard" left="23%" />
+        </Pricing>
+
+        <div className="w-full my-5">
+          <Button
+            textColor="text-white"
+            bgColor="bg-black"
+            className="mx-auto text-xl"
+          >
+            See Pricing
+          </Button>
+        </div>
+      </Section>
+      {/* End Pricing Table */}
     </div>
   );
 };
