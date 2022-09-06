ef3323ae198662dd93921b80ca6e5eb96fd7d29b
SohaibAshraf <sohaibbutt57@gmail.com>
2022-09-06 12:41:43 +0500

    react player package installed

--- components/Fawwaz/Card/CardContent.tsx
+++ components/Fawwaz/Card/CardContent.tsx
@@ -5,7 +5,6 @@ import IconItem from "./IconItem"
 import { CardContent } from "../component.type";
 
 
-
 const CardContent: FC<CardContent> = ({ className, children }) => {
 	return (
 		<div className={`px-7 mt-4 ${className ? className : ''} `}>
