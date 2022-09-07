0db44a0fdabcd40043af406a1694d56655ea37ba
jawwad-ali <alijawwad001@gmail.com>
2022-09-07 14:20:49 +0500

    added partners sections

--- pages/views/cms-development-services.tsx
+++ pages/views/cms-development-services.tsx
@@ -1,4 +1,5 @@
 import Hero from "../../components/Fawwaz/Sections/Hero";
+import Section from "../../components/Fawwaz/Sections/Section";
 import { NextPage } from "next";
 import Image from "next/image";
 import Rectangle from "../../components/Rectangle";
@@ -6,24 +7,31 @@ import IconSaleFunnel from "../../components/assets/icon-sales-funnel.png";
 import Group from "../../components/assets/group.png";
 import Trophy from "../../components/assets/icon-trophy-big-1.png";
 import CloudComputing from "../../components/assets/icon-cloud-computing-1.png";
+import Logo1 from "../../components/assets/logo-1.png";
+import Logo2 from "../../components/assets/logo-2.png";
+import Logo4 from "../../components/assets/logo-4.png";
+import Logo5 from "../../components/assets/logo-5.png";
+import Logo6 from "../../components/assets/logo-6.png";
+import Logo7 from "../../components/assets/logo-7.png";
+import Logo from "components/Fawwaz/Logo";
 
 const Page: NextPage = () => {
   return (
     <div>
       {/* First Section */}
       {/* Left Side */}
-      <div className="bg-black/[0.7] flex justify-center">
+      <div className="bg-black/[0.7] flex flex-col xl:flex-row justify-center">
         <div>
           <Hero
             title="CMS Development Services that are completely transparent"
             description="Content management systems (CMS) are a must-have for all websites, from large informational sites to e-commerce stores. The simplest way to ensure a proper web presence is to streamline and organize your website, and Muscled can assist. Our CMS development services begin at $2,200 and are completely transparent. Scroll down for more information, or contact us for a specific quote!"
-            input
+            input 
           />
         </div>
 
         {/* Right Side */}
-        <div className="max-w-[640px] mt-20 max-h-[640px] flex flex-col  justify-center">
-          <div className="my-1">
+        <div className="max-w-[640px] xl:mt-20 -mt-24 max-h-[640px] flex flex-col justify-center">
+          <div className="my-1 bg-blue-600 mx-auto">
             <Rectangle
               icon={IconSaleFunnel}
               text1={"Access the data-fueled plans behind "}
@@ -31,7 +39,7 @@ const Page: NextPage = () => {
               text2={"IN CLIENT REVENUE"}
             />
           </div>
-          <div className="my-1">
+          <div className="my-1 mx-auto">
             <Rectangle
               icon={Group}
               text1={"Use the expert-led tactics behind"}
@@ -39,7 +47,7 @@ const Page: NextPage = () => {
               text2={"IN LEADS FOR OUR CLIENTS"}
             />
           </div>
-          <div className="my-1">
+          <div className="my-1 mx-auto">
             <Rectangle
               icon={Trophy}
               text1={"Unlock do-it-for-me marketing with"}
@@ -47,7 +55,7 @@ const Page: NextPage = () => {
               text2={"DIGITAL MARKETING EXPERTS"}
             />
           </div>
-          <div className="my-1">
+          <div className="my-1 mx-auto">
             <Rectangle
               icon={CloudComputing}
               text1={"Accelerate ROI with"}
@@ -56,6 +64,22 @@ const Page: NextPage = () => {
             />
           </div>
         </div>
+      </div>
+
+      {/* Partner section */}
+      <div className="mb-5 pb-5">
+        <Section className="text-center pt-16">
+          <h3 className="text-black-50 text-2xl">Our Partners</h3>
+
+          <div className="pt-5 flex flex-wrap justify-between pb-12 gap-6">
+            <Logo src={Logo1} width={154} height={42} />
+            <Logo src={Logo2} width={154} height={42} />
+            <Logo src={Logo4} width={154} height={42} />
+            <Logo src={Logo5} width={154} height={42} />
+            <Logo src={Logo6} width={154} height={42} />
+            <Logo src={Logo7} width={154} height={42} />
+          </div>
+        </Section>
       </div>
     </div>
   );
--- pages/views/muscled-d2c-social-advertising-services.tsx
+++ pages/views/muscled-d2c-social-advertising-services.tsx
@@ -9,7 +9,7 @@ import Logo from "components/Fawwaz/Logo";
 
 	return (<div>
         {/* First Section */}
-			<div className=" bg-black-70 px-3 lg:px-0">
+			<div className=" bg-black-70 px-3 lg:px-0"> 
 				<Hero
 					title="With Muscled D2C Social Advertising Services, you can attract, engage, and sell."
 					description="With over 5 billion active users on social media today, social platforms are prime territory for reaching a large direct-to-consumer (D2C) audience. If you sell B2C products, social ads increase your online reach, engage customers, and help you sell, all of which contribute to your bottom line. Continue reading to find out how Muscled fyi, a Meta Business Partner agency, can drive results with our direct-to-consumer social media advertising services!"
@@ -18,7 +18,7 @@ import Logo from "components/Fawwaz/Logo";
 				/>
 			</div>
         {/* Our partner section */}
-        <Section className="text-center pt-16">
+        <Section className="text-center pt-16"> 
             <h3 className="text-black-50 text-2xl">Our Partners</h3>
 
             <div className="pt-5 flex flex-wrap justify-between pb-12 gap-6">
