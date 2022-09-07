b5b757fa3d0a227af2c5cc19c577d1fb053369fb
RehanRabbani <rabbanirehan6@gmail.com>
2022-09-08 03:18:14 +0500

    [MUS-00018] updated

--- components/common/Testimonials.tsx
+++ components/common/Testimonials.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import Section from "../Fawwaz/Sections/Section";
 import Button from "../Fawwaz/Button";
-import VideoFrame from "../Fawwaz/VideoFrame";
+import ReactPlayerModal from "components/ReactPlayerModal";
+import PlayIcon from "components/playIcon";
 
 interface ReusableTestimonialsProps {
   testimonialData: { para1?: string; para2?: string; buttonLabel?: string };
@@ -11,7 +12,7 @@ const ReusableTestimonials: FC<ReusableTestimonialsProps> = ({
 }) => {
   return (
     <div className="bg-black">
-      <Section className="flex flex-wrap justify-center py-20 px-3 gap-32">
+      <Section className="flex justify-center py-20 px-3 gap-32">
         <div className="max-w-[560px] text-accent-white">
           <p className="text-xl md:text-2xl mb-8">{para1}</p>
           <p className="font-bold text-base md:text-xl mb-14">{para2}</p>
@@ -20,9 +21,13 @@ const ReusableTestimonials: FC<ReusableTestimonialsProps> = ({
             {buttonLabel}
           </Button>
         </div>
-        <div className="w-[500px] min-h-[400px] h-full bg-white-gray relative">
-          <VideoFrame className="w-full h-full absolute" allowFullScreen />
-        </div>
+
+        <ReactPlayerModal
+          url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          width="590px"
+          height="400px"
+          Icon={PlayIcon}
+        />
       </Section>
     </div>
   );
