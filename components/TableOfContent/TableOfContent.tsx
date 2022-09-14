164babf025ed34261f6816bfe08050248a841cd6
BMuscled <bilal.muscled@gmail.com>
2022-09-15 01:58:07 +0500

    Added UX-Analysis/Abdlrhman Table of Contents Functionality

--- components/TableOfContent/TableOfContent.tsx
+++ components/TableOfContent/TableOfContent.tsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import { FC } from "react";
+import {ChevronDoubleRightIcon, ChevronDownIcon } from "@heroicons/react/solid";
 
-import { FaListAlt } from "react-icons/fa";
-import { BiChevronRight } from "react-icons/bi";
-import { HiChevronDoubleRight } from "react-icons/hi";
 interface TableProps {
   list: string[];
 }
+
 const TableOfContent: FC<TableProps> = ({ list }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+ 
+  function toggle() {
+    setIsOpen(!isOpen);
+  }
+  
   return (
     <>
       {/* header */}
-      <div className="border-2 border-solid border-slat-500 rounded-2xl	 w-[440px]">
-        <div className="bg-[#F2F2F2] text-white py-2 px-8 flex justify-between items-center">
-          <h2 className="flex items-center text-lg gap-2 font-bold	">
-            <FaListAlt size={20} />
+      <div className="border-[1px] border-solid border-black-30 rounded-[10px] w-[440px]">
+        <div className="bg-[#F2F2F2] text-white py-2 px-8 flex justify-between items-center h-[60px] rounded-[10px]">
+          <h2 className="flex items-center text-xl gap-[10px] font-bold text-black-80">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="24" height="24" rx="5" fill="black" fill-opacity="0.8"/>
+<path d="M20 9.77778H7.55556V8H20V9.77778ZM20 13.3333H7.55556V11.5556H20V13.3333ZM20 16.8889H7.55556V15.1111H20V16.8889ZM5.77778 16.8889H4V15.1111H5.77778V16.8889ZM5.77778 8V9.77778H4V8H5.77778ZM5.77778 13.3333H4V11.5556H5.77778V13.3333Z" fill="white"/>
+</svg>
+
             Table of Contents
           </h2>
-          <div>
-            <BiChevronRight className="w-10 h-10 rotate-90 text-black" />
+          <div onClick={toggle} className="cursor-pointer">
+            <ChevronDownIcon className={`w-9 h-9 transition-transform text-black-80 ${
+            isOpen ? "rotate-0" : "-rotate-180"} `} />
           </div>
         </div>
 
-        <ul className="bg-white text-left px-8 py-2 rounded-b-2xl	">
-          {list.map((e) => {
+        <ul className={`bg-white  pl-1 py-2 rounded-[10px] ${
+          isOpen ? "animate-slide bg-white" : "animate-slideUp bg-[#FAE8E8]"} `}>
+            
+          { isOpen && list.map((e) => {
             return (
-              <li className="flex items-center text-gray-800 font-light py-2">
-                <HiChevronDoubleRight />
-                <span className="px-2">{String(e)}</span>
+              <li className="flex items-center text-black-100 font-light py-2 gap-2">
+                <div>
+                <ChevronDoubleRightIcon className="fill-black-80 w-5 h-5" />
+                </div>
+                <p className="text-base leading-[24px]">{e}</p>
               </li>
             );
           })}
