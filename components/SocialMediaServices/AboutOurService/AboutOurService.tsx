26ea929f0f176b8598eba71278bbf81dbb942a2f
Muzamil Shakir <mshakir39@gmail.com>
2022-09-15 01:55:38 +0500

    almost all section added of social media services instead of see result with social media section and additional social media marketing section

---
+++ components/SocialMediaServices/AboutOurService/AboutOurService.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function AboutOurServiceSection() {
+  return (
+    <div className='lg:px-20 md:px-12 px-4 py-20 bg-[#C40000] bg-opacity-[0.06] mt-[120px] flex flex-col justify-center items-center'>
+      <p className='md:text-[40px] text-2xl font-bold md:leading-[52px] leading-[52px] text-center  max-w-[1000px]'>
+        Our social media services
+      </p>
+      <p className='md:text-xl text-base xl:leading-[30px] leading-[30px] text-center mt-5 max-w-[1000px]'>
+        To complement a results-driven content marketing strategy on social
+        media, our Social Media Plans provide advertising management for boosted
+        posts exclusively. Other ads, such as traffic ads, lead ads, shopping
+        feed ads, marketplace ads, etc., are not included in the advertising. In
+        addition to the content management campaign, any additional ads—aside
+        from boosted posts and sponsored content—that are produced or exceed the
+        limit ad spend level require a full social media advertising campaign.
+      </p>
+    </div>
+  );
+}
--- pages/views/social-media-services.tsx
+++ pages/views/social-media-services.tsx
@@ -1,27 +1,35 @@
-import React from "react";
-import { NextPage } from "next";
-import SEOMarketingSVG from "components/assets/svg/SEOMarketingSVG";
+import React from 'react';
+import { NextPage } from 'next';
+import Hero from '../../components/SocialMediaServices/Hero/Hero';
+import OurPartner from '@/components/landingPage/OurPartner/OurPartner';
+import Pricing from '@/components/SocialMediaServices/PricingPlan/Pricing';
+import Testimonial from '@/components/SocialMediaServices/Testimonials/Testimonial';
+import PromotionalSection from '@/components/landingPage/PromotionalSection/PromotionalSection';
+import ServiceTable from '@/components/SocialMediaServices/ServiceTable/ServiceTable';
+import AboutOurServiceSection from '@/components/SocialMediaServices/AboutOurService/AboutOurService';
+import Faq from '@/components/SocialMediaServices/FAQ/FAQ';
+import TableOfContext from '@/components/SocialMediaServices/TableOfContext/TableOfContext';
 
-import Hero from '../../components/Fawwaz/Sections/Hero';
-import OurPartners from '../../components/Fawwaz/Sections/OurPartners';
-import PricingPlan from '../../components/Fawwaz/Sections/PricingPlan';
+const Page: NextPage = () => {
+  return (
+    <>
+      <Hero />
+      <OurPartner />
+      <Pricing />
+      {/* <PromotionalSection /> */}
+      <Testimonial />
+      <ServiceTable />
+      <AboutOurServiceSection />
+      <div className='mt-[100px] px-5 md:px-10 lg:px-20 mx-auto container gap-[60px] lg:flex items-start pb-12 relative'>
+        <div className='lg:w-[60%] mt-[20px]'>
+          <Faq />
+        </div>
+        <div className='lg:w-[40%] sticky top-0 pt-[20px]'>
+          <TableOfContext />
+        </div>
+      </div>
+    </>
+  );
+};
 
-
- const Page: NextPage = () => {
-
-	return (
-        <>
-            <Hero 
-                title={"Landing Page Design Services for PPC Campaigns"} 
-                description = {''} 
-                // heroSVG = {FunnelSVG}
-                input 
-            />
-            <OurPartners />
-            <PricingPlan />
-        </>
-    )
-}
-
-
-export default Page
+export default Page;
