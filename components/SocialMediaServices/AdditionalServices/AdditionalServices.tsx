50a655445b4055f1ae6d84db345d46099a99dc17
Muzamil Shakir <mshakir39@gmail.com>
2022-09-16 19:32:45 +0500

    remove comment

--- components/SocialMediaServices/AdditionalServices/AdditionalServices.tsx
+++ components/SocialMediaServices/AdditionalServices/AdditionalServices.tsx
@@ -73,8 +73,6 @@ export default function AdditionalServices() {
                     d='M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75'
                   />
                 </svg>
-                {/* 
-                <AiOutlineArrowRight className='text-white' size={17} /> */}
               </button>
             </div>
           </>
