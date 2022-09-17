12072242c5df7a048afa54fb15fa80b501038334
Priyanshu Bhandari <technopriyu@gmail.com>
2022-09-17 20:29:59 +0530

    fixed partner section

--- components/Rainmaker/Partners.tsx
+++ components/Rainmaker/Partners.tsx
@@ -7,25 +7,27 @@ import Logo6 from './assets/Logo6logo.png';
 import Logo7 from './assets/Logo7logo.png';
 const Partners = () => {
   return (
-    <section className="text-center py-8">
-      <h3 className="text-black/50 text-[1.5rem] mb-4">Our Partners</h3>
-      <div className="flex items-center justify-center px-8 lg:px-28 flex-shrink-0 gap-x-10 mt-4 pb-4">
-        <div className="relative flex-shrink-0">
+    <section className="text-center bg-white py-[3.125rem] px-[6.25rem]">
+      <h3 className="text-black/50 text-[1.5rem] mb-4 font-bold leading-[130%]">
+        Our Partners
+      </h3>
+      <div className="max-w-[77.5rem] mx-auto flex items-center justify-center px-8 lg:px-28 gap-x-[3.2rem] xl:gap-x-[4.5rem] mt-4 pb-4">
+        <div className="relative flex-shrink-0 max-w-[9.875rem]">
           <Image src={Logo1} alt="companyName" objectFit="contain" />
         </div>
-        <div className="relative flex-shrink-0">
+        <div className="relative flex-shrink-0 max-w-[9.875rem]">
           <Image src={Logo2} alt="companyName" objectFit="contain" />
         </div>
-        <div className="relative flex-shrink-0">
+        <div className="relative flex-shrink-0 max-w-[9.875rem]">
           <Image src={Logo4} alt="companyName" objectFit="contain" />
         </div>
-        <div className="relative flex-shrink-0 hidden xl:block">
+        <div className="relative flex-shrink-0 hidden xl:block max-w-[9.875rem]">
           <Image src={Logo5} alt="companyName" objectFit="contain" />
         </div>
-        <div className="relative flex-shrink-0 hidden lg:block">
+        <div className="relative flex-shrink-0 hidden lg:block max-w-[9.875rem]">
           <Image src={Logo6} alt="companyName" objectFit="contain" />
         </div>
-        <div className="relative flex-shrink-0 hidden lg:block">
+        <div className="relative flex-shrink-0 hidden lg:block max-w-[9.875rem]">
           <Image src={Logo7} alt="companyName" objectFit="contain" />
         </div>
       </div>
