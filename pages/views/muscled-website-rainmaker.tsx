8da71f002ef723de704954604fe0aabc51b50b38
BMuscled <bilal.muscled@gmail.com>
2022-09-20 20:00:41 +0500

    NextPage type added in Rainmaker main page file

--- pages/views/muscled-website-rainmaker.tsx
+++ pages/views/muscled-website-rainmaker.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NextPage } from "next";
 import HeroSection from '@/components/Rainmaker/HeroSection';
 import Partners from '@/components/Rainmaker/Partners';
 import PromotionSection from '@/components/Rainmaker/PromotionSection';
@@ -6,7 +7,7 @@ import FeaturedSection from '@/components/Rainmaker/FeaturedSection';
 import ResultPromotion from '@/components/Rainmaker/ResultPromotion';
 import GuidesSection from '@/components/Rainmaker/GuidesSection';
 import BenefitsSection from '@/components/Rainmaker/BenefitsSection';
-const MuscledWebsiteRainmaker: React.FC = () => {
+const MuscledWebsiteRainmaker:NextPage = () => {
   return (
     <div>
       <HeroSection />
