0b79880317e3ae54e11f417356299a6aba0fe0d4
BMuscled <bilal.muscled@gmail.com>
2022-10-06 01:44:48 +0500

    landingpage guide details component commented out

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ import OurPartner from "./OurPartner/OurPartner";
 import Pricing from "./PricingPlan/Pricing";
 import Testimonial from "./Testimonials/Testimonial";
 import PromotionalSection from "./PromotionalSection/PromotionalSection";
-import GuideDetails from "./GuideDetails/GuideDetails";
+// import GuideDetails from "./GuideDetails/GuideDetails";
 import Faq from "./FAQ/FAQ";
 import TableOfContext from "./TableOfContext/TableOfContext";
 import ServiceTable from "./ServiceTable/ServiceTable";
@@ -20,7 +20,7 @@ export default function LandingPage() {
 			<ServiceTable />
 			<div className="mt-[100px] px-5 md:px-10 lg:px-20 mx-auto container gap-[60px] lg:flex items-start pb-12 relative">
 				<div className="lg:w-[60%] mt-[20px]">
-					<GuideDetails />
+					{/* <GuideDetails /> */}
 					<Faq />
 				</div>
 				<div className="lg:w-[40%] sticky top-0 pt-[20px]">
