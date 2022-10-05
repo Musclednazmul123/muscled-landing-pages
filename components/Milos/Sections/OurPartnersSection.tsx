2ec89fc866398ddd1bd5e5e180e9a35c5b556ba1
BMuscled <bilal.muscled@gmail.com>
2022-10-06 01:54:31 +0500

    logos Casing changed

--- components/Milos/Sections/OurPartnersSection.tsx
+++ components/Milos/Sections/OurPartnersSection.tsx
@@ -5,8 +5,8 @@ import logo1 from "../../assets/logo/logo1.png"
 import logo2 from "../../assets/logo/logo2.png"
 import logo4 from "../../assets/logo/logo4.png"
 import logo5 from "../../assets/logo/logo5.png"
-import logo6 from "../../assets/logo/Logo6.png"
-import logo7 from "../../assets/logo/Logo7.png"
+import logo6 from "../../assets/logo/logo6.png"
+import logo7 from "../../assets/logo/logo7.png"
 
 const OurPartnersSection = () => {
 	return (
