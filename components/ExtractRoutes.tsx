2c056c0b25f6b97b10111b8f465e267f855a467e
Nazmul Hawlader <nazmul.muscled@gmail.com>
2022-11-07 23:35:29 +0600

    breadcrumb is fixed

--- components/ExtractRoutes.tsx
+++ components/ExtractRoutes.tsx
@@ -5,40 +5,19 @@ const ExtractRoutes:FC = () => {
     const router=useRouter();
     const routesArray=router.asPath;
 
-    const breadcrumb = routesArray.replace("/", " > ")
-
-
-    // routesArray.pop();
-    // routesArray.splice(0,1,"Home")
-    // console.log(routesArray); 
-    // let uperCasedRoutes=routesArray.map((route)=>{
-
-    //     const capitalized=route.includes("-")? route.split("-").map((route)=>{
-    //         return route.replace(/^./,route[0].toUpperCase());
-    //     }).join(" "):
-    //     route.replace(/^./,route[0].toUpperCase());
-    // return capitalized;
-    // })
-
-    // console.log(uperCasedRoutes);
-
-    // let transformedRoutes=uperCasedRoutes.join(">").split("");
-    console.log(breadcrumb);
+    let bc = "Home"
+    const pageroute = routesArray.split("/").map((e, i)=>{
+      if(i==0){
+        bc += `${e}`
+      } else {
+        bc += ` > ${e}`
+      }
+      
+    })
 
   return (
     <div className='flex space-x-2 text-white'>
-      <p className='font-[500] text-lg'>Home {breadcrumb}</p>
-        {/* {
-            uperCasedRoutes?.map((route,index)=>{
-                return (
-                    index!==uperCasedRoutes.length-1?
-                    <div key={`${index}-breadcrumb-${index}`}>
-                    <p className='font-[500] text-lg'>{route}</p>
-                    <p className='font-[500] text-lg'>{">"}</p>
-                    </div>:<p key={`${index}-breadcrumb-${index}`} className='font-[500] text-lg'>{route}</p>
-                )
-            })
-        } */}
+      <p className='font-[500] text-lg'>{bc}</p>
         </div>
   )
 }
